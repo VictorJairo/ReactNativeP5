e2737563f1855cf9f3dda0b18dca94c344f12f8c
VictorJairo <victorjairo1893@gmail.com>
2025-10-09 15:43:33 -0300

    enviando

--- Aula02/App.js
+++ Aula02/App.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import LayoutResponsivo from './src/components/LayoutResponsivo'
+import CartaoPerfil from './src/components/CartaoPerfil'
 
 const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <LayoutResponsivo />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
+      <CartaoPerfil />
     </View>
   )
 }
--- Aula02/src/components/LayoutResponsivo.js
+++ Aula02/src/components/LayoutResponsivo.js
@@ -7,42 +7,24 @@ const LayoutResponsivo = () => {
         <View style={styles.header}>
             <Text>Cabeçalho</Text>
         </View>
-        
+
         <View style={styles.content}>
             <Text>Conteúdo Principal</Text>
         </View>
 
-         <View style={styles.footer}>
+        <View style={styles.footer}>
             <Text>Rodapé</Text>
         </View>
-        
     </View>
   )
 }
 
+
 const styles = StyleSheet.create({
-    container: { flex: 1},
-
-    header: {
-        flex: 1,
-        backgroundColor: 'lightblue',
-        justifyContent: 'center',
-        alignItems: 'center' },
-
-    content: {
-         flex: 3,
-          backgroundColor: 'white',
-           justifyContent: 'center',
-            alignItems: 'center'},
-
-    footer: {
-        flex: 1,
-        backgroundColor: 'lightyellow',
-        justifyContent: 'center',
-        alignItems: 'center'},
-
-
+    container: { flex: 1 },
+    header: { flex: 1, backgroundColor: 'lightblue', justifyContent: 'center', alignItems: 'center' },
+    content: { flex: 3, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' },
+    footer: { flex: 1, backgroundColor: 'lightgray', justifyContent: 'center', alignItems: 'center' }
 })
-
 
 export default LayoutResponsivo
